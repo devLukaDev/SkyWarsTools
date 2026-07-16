096e1c2a6d89944b05e4d49a9dc38d09f4c20fa4
Luka de Vrij <35869162+LukaDeVrij@users.noreply.github.com>
2026-07-16 23:52:51 +0200

    2 modeules working

--- swtools/app/components/player/tabs/gameplay/Perks.tsx
+++ swtools/app/components/player/tabs/gameplay/Perks.tsx
@@ -1,3 +1,4 @@
+import PortalTooltip from "@/app/components/universal/MinecraftTooltip";
 import { OverallResponse } from "@/app/types/OverallResponse";
 import MinecraftText from "@/app/utils/MinecraftText";
 import { perks } from "@/app/utils/Perks";
@@ -15,46 +16,33 @@ function Perks({ response }: { response: OverallResponse }) {
 						return <></>;
 					}
 					return (
-						<div
-							key={perkObj.name}
-							className="flex items-center justify-center h-35 w-35 bg-cover bg-center relative"
-							style={{
-								backgroundImage: `url('/ranked/invSlot.png')`,
-							}}
+						<PortalTooltip
+							key={perk}
+							trigger={({ ref, onMouseEnter, onMouseLeave, onFocus, onBlur, tabIndex }) => (
+								<div
+									ref={ref}
+									className="flex items-center justify-center h-35 w-35 bg-cover bg-center"
+									style={{ backgroundImage: `url('/ranked/invSlot.png')` }}
+									onMouseEnter={onMouseEnter}
+									onMouseLeave={onMouseLeave}
+								>
+									<img
+										src={"/perks/" + perkObj.icon}
+										height={120}
+										width={120}
+										style={{ imageRendering: "pixelated" }}
+										alt=""
+										tabIndex={tabIndex}
+										onFocus={onFocus}
+										onBlur={onBlur}
+									/>
+								</div>
+							)}
 						>
-							<img
-								src={"/perks/" + perkObj.icon}
-								height={120}
-								width={120}
-								style={{ imageRendering: "pixelated" }}
-								className="peer"
-								alt=""
-								tabIndex={0}
-								onClick={(e) => {
-									const tooltip = e.currentTarget.nextSibling as HTMLElement;
-									if (tooltip) {
-										tooltip.classList.toggle("opacity-100");
-										tooltip.classList.toggle("pointer-events-auto");
-									}
-								}}
-								onBlur={(e) => {
-									const tooltip = e.currentTarget.nextSibling as HTMLElement;
-									if (tooltip) {
-										tooltip.classList.remove("opacity-100");
-										tooltip.classList.remove("pointer-events-auto");
-									}
-								}}
-							/>
-							<div
-								className="fixed lg:absolute top-full left-[-1/10] mt-2 w-full lg:w-100 p-2 rounded items-center justify-center opacity-0 group-hover:opacity-100 peer-hover:opacity-100 transition-opacity bg-black/90 z-10 text-xl text-white text-left pointer-events-none group-focus-within:opacity-100 group-focus-within:pointer-events-auto"
-								tabIndex={-1}
-							>
-								{/* <MinecraftText>{perkObj.name}</MinecraftText> */}
-								{Object.values(perkObj.lore).map((loreLine, index) => (
-									<MinecraftText key={index}>{loreLine}</MinecraftText>
-								))}
-							</div>
-						</div>
+							{Object.values(perkObj.lore).map((loreLine, index) => (
+								<MinecraftText key={index}>{loreLine}</MinecraftText>
+							))}
+						</PortalTooltip>
 					);
 				})
 			) : (
